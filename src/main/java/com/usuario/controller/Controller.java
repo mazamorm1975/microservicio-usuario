6a0eecaf86852112c65850e64861d948a3007fea
Mario Zamora <mzamora114@hotmail.com>
2025-02-05 11:10:57 -0600

    agregación de linea de prueba

--- src/main/java/com/usuario/controller/Controller.java
+++ src/main/java/com/usuario/controller/Controller.java
@@ -79,6 +79,7 @@ public class Controller {
 
 	// Se genera API Rest para llamar al servicio que conectara con el
 	// microservice_coche
+	//hola que tal, este es un TEST
 	@GetMapping("/coche/{usuarioId}")
 	public ResponseEntity<List<Carro>> consigueVehiculosPorUsuarioId(@PathVariable("usuarioId") int usuarioId) {
 
